3ea9149531a9b6c286e93258fd3efa611a663478
azimjonalijonov <115147667+azimjonalijonov@users.noreply.github.com>
2023-04-29 00:33:53 +0500

    Update SubsetOfSquaresCreator.java

--- src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
@@ -1,9 +1,19 @@
 package com.epam.mjc.collections.set;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class SubsetOfSquaresCreator {
     public Set<Integer> createSubsetOfSquares(List<Integer> sourceList, int lowerBound, int upperBound) {
+        Set<Integer> set = new HashSet<>();
+        int temp;
+        for (Integer e : sourceList) {
+            temp = e * e;
+            if (lowerBound <= temp && upperBound >= temp) {
+                set.add(temp);
+            }
+        }
+        return set;
     }
 }
