641b6cb9e436accad1ef9d53eb7a37960cae2288
azimjonalijonov <115147667+azimjonalijonov@users.noreply.github.com>
2023-04-29 00:31:57 +0500

    Update HashSetCreator.java

--- src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
+++ src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
@@ -5,5 +5,25 @@ import java.util.List;
 
 public class HashSetCreator {
     public HashSet<Integer> createHashSet(List<Integer> sourceList) {
+        HashSet<Integer> result = new HashSet<>();
+        int temp = 0;
+        for (int i = 0; i < sourceList.size(); i++) {
+            temp = sourceList.get(i);
+            if (isEven(temp)) {
+                result.add(temp);
+                while (isEven(temp)) {
+                    temp /= 2;
+                    result.add(temp);
+                }
+            } else {
+                result.add(temp);
+                result.add(temp * 2);
+            }
+        }
+        return result;
+    }
+
+    private boolean isEven(int temp) {
+        return temp % 2 == 0;
     }
 }
